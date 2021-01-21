4c3a5ab71b7b17f2a6309c4ab4712f0ac6ca6c70
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-21 15:18:05 -0600

    commit tree 9bd34b3505da2fa633a1fe786dc1b9fba3389221

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,19 +5,14 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
-    if @post.valid?
-      @post.save
+  end
+
+  def update
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
     end
-  end
-
-  def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
   end
 
   private
